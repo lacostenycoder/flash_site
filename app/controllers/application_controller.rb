c46c649f1fd31fd8a22f4267a4b7eb042b68244d
jayanti <jayanti@vinsol.com>
2018-03-11 12:48:55 +0530

    add remember me functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,13 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    if cookies.signed[:user_id]
+      id = cookies.signed[:user_id]
+    elsif session[:user_id]
+      id = session[:user_id]
+    end
+    @current_user ||= User.find_by(id: id)
   end
+
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-
   before_action :redirect_when_logged_in, only: [:new, :create]
   before_action :find_user, only: :create
 
@@ -9,6 +8,9 @@ class SessionsController < ApplicationController
   def create
     if @user && @user.authenticate(params[:password])
       unless @user.confirm_token
+        if params[:remember_me]
+          cookies.permanent.signed[:user_id] = @user.id
+        end
         flash[:success] = t('.success')
         session[:user_id] = @user.id
         redirect_to root_url
@@ -24,6 +26,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    cookies.delete :user_id
     flash[:success] = t('.logged_out')
     redirect_to root_url
   end
